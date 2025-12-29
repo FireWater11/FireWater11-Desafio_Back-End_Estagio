783c4e8ab6d368d8606dfc42605c85cd6377a0a0
Luidi Augusto <luidi.augusto1234@gmail.com>
2025-12-28 23:22:49 -0300

    novo service -> metodo cadastrarProduto

---
+++ src/main/java/com/loja_starwars/demo/service/ProdutosService.java
@@ -0,0 +1,46 @@
+package com.loja_starwars.demo.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.loja_starwars.demo.infraestructure.domain.ProdutosDomain;
+import com.loja_starwars.demo.infraestructure.dto.ProdutoDTO;
+import com.loja_starwars.demo.infraestructure.repositories.ProdutoRepository;
+
+@Service
+
+public class ProdutosService {
+
+    @Autowired
+    private ProdutoRepository produtos;
+
+    private boolean isVazio(String valor) {
+        return valor == null || valor.isBlank();
+    }
+
+    public ProdutosDomain cadastrarProduto(ProdutoDTO dto) {
+
+        if (isVazio(dto.getDate()) || 
+        isVazio(dto.getTitle()) || 
+        isVazio(dto.getZipcode()) || 
+        isVazio(dto.getThumbnailHd())) {
+            throw new RuntimeException("As informações não podem estar vazias.");
+        }
+        
+        if (dto.getPrice() < 0) {
+            throw new RuntimeException("O preço deve ter valor positivo.");
+        }
+
+        ProdutosDomain novoProduto = new ProdutosDomain(
+            null,
+            dto.getTitle(),
+            dto.getPrice(),
+            dto.getZipcode(),
+            dto.getSeller(),
+            dto.getThumbnailHd(),
+            dto.getDate()
+        );
+        
+        return produtos.save(novoProduto);
+    }
+}
